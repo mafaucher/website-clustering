df70d77117b14f6b4c7e103084eba1f428a3040c
ma.faucher <ma.faucher@f53481e0-e03f-27c9-09c0-0f95bf841db5>
2011-12-12 18:52:24 +0000

    VectorSpace integrated with clustering. main.py contains sample code

--- main.py
+++ main.py
@@ -11,24 +11,46 @@ class Usage(Exception):
         self.msg = msg
 
 def main(argv=None):
-    """# Create Index
+    # Sample: Indexing the collection
+    """
     index = ii.InvertedIndex()
     indexer = wi.WebIndexer()
     tokeniser = tk.Tokeniser()
     indexer.spimi(index, tokeniser)
     """
-    # Load Index
+    # Sample: Loading the index (don't need to if you just indexed, see above)
     index = ii.InvertedIndex()
     ii.load("index/fullindex.csv", index)
     indexer = wi.WebIndexer()
-    wi.load("index/doclength.csv", indexer.docL)
+    indexer.load()
     
+    # Sample: Generating the vector space
     vSpace = vs.VectorSpace(index, indexer)
     vSpace.buildVectors()
-    for i in range(vSpace.numberOfTerms):
-        print 0, vSpace.vectorIndex[0, i]
-        print 1, vSpace.vectorIndex[1, i]
-        print 'c', vSpace.centroid([0,1])[i]
+    
+    # Sample: Simple K-means
+    k = 3
+    # w: List of K clusters [ [docId, docId, ...], [docId, docId, ...], [docId, docId, ...] ]
+    # u: List of K centroids [ wi.centroid(w[0]), ..., wi.centroid(w[k-1]) ]
+    # rss: total RSS value for this clustering scheme
+    w, u, rss = vSpace.kMeans(k)
+
+    # Sample: K-means with the smallest RSS using N different seeds
+    k = 3
+    n = 10
+    w, u, rss = vSpace.kMeansBestOfN(k, n)
+
+    # Sample: Query
+    tokeniser = tk.Tokeniser()
+    userInput = raw_input("> ").strip()
+    terms = tokeniser.tokenise(userInput)
+    queryVector = vSpace.buildQueryVector(terms)
+    docList = vSpace.nearestCluster(w, u, queryVector)
+
+    # Sample: getting a list of URLs from a list of doc IDs
+    urlList = [indexer.urls[docId] for docId in docList]
+
+    print urlList
 
 if __name__ == "__main__":
     sys.exit(main())
